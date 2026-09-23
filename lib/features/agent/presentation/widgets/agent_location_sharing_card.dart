356ed13addc36d60ba8a7918afc77ceeb931f878
Pranav Nimje <pranavnimje7@gmail.com>
2026-09-23 17:25:02 +0530

    Update Android launcher icons and adaptive icon assets for QuickServe branding

--- lib/features/agent/presentation/widgets/agent_location_sharing_card.dart
+++ lib/features/agent/presentation/widgets/agent_location_sharing_card.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:latlong2/latlong.dart';
-import '../../../../core/utils/formatters.dart';
 import '../../../../repositories/repository_providers.dart';
 import '../../../../services/geolocation_service.dart';
 import '../controllers/agent_location_controller.dart';
